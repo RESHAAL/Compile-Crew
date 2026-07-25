5036b3bd354588636fa43aabb89dd47a1b04a1ad
namratavaidya <namrata.vaidya2006@gmail.com>
2026-07-25 17:03:47 +0530

    Delete check_dataset.py

--- check_dataset.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DATASET_PATH = r"C:\Users\Namrata Vaidya\.cache\kagglehub\datasets\abdallahalidev\plantvillage-dataset\versions\3\plantvillage dataset\color"
-
-print("Dataset classes:")
-
-for item in os.listdir(DATASET_PATH):
-    print(item)
